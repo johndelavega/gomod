8db7d730785b4589303cd6f7010065b60f8c0cea
John de la Vega <john.dlvg@gmail.com>
2019-02-21 11:26:57 -0800

    gomod v0.0.2 - updated for usage info

--- gomod.go
+++ gomod.go
@@ -2,8 +2,9 @@ package gomod
 
 import "fmt"
 
-// Gomod use for testing replace and local modules
+// Gomod use for testing replace and local modules/packages
 func Gomod() string {
-	fmt.Println("Gomod() - import 'github.com/johndelavega/gomod'")
-	return "Gomod() - import 'github.com/johndelavega/gomod v0.0.1'"
+	fmt.Println("Gomod v0.0.2 - main.go import anymod 'github.com/johndelavega/gomod'")
+	fmt.Println("go.mod - replace github.com/johndelavega/gomod v0.0.2 => ./anymod")
+	return "Gomod v0.0.2 - main.go import anymod 'github.com/johndelavega/gomod'"
 }
