0295a46f020f63a51645bcd003f1ecebb5f73bd3
John de la Vega <john.dlvg@gmail.com>
2019-02-21 11:02:46 -0800

    gomod v0.0.0 - init commit

---
+++ gomod.go
@@ -0,0 +1,9 @@
+package gomod
+
+import "fmt"
+
+// Gomod use for testing replace and local modules
+func Gomod() string {
+	fmt.Println("Gomod() - import 'github.com/johndelavega/gomod'")
+	return "Gomod() - import 'github.com/johndelavega/gomod'"
+}
